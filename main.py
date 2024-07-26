529333d2f20da81c8649579fada1b560640eadad
Shayan najafi <najafishsyan492@gmail.com>
2024-07-26 23:02:06 +0330

    initilized class and base functions

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 
+
+
+
+class main():
+    def __init__(self) -> None:
+        pass
+
+    def removing_black_and_white_images(self):
+        pass
+
+    def face_detection(self):
+        pass
+
+    def face_feature_extraction(self):
+        pass
+
+    def is_frontpose(self):
+        pass
+
+    def 
+        
+
+
